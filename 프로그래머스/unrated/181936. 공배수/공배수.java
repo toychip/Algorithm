54ed69eb87784fbb40f96242f11e5eb9f214090a
Junhyoung <109949924+toychip@users.noreply.github.com>
2023-05-04 15:26:55 +0900

    [unrated] Title: 공배수, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.java
+++ 프로그래머스/unrated/181936. 공배수/공배수.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int number, int n, int m) {
-        int answer = 0;
         if((number % n == 0) && (number % m ==0)){
             return 1;
         }else{
